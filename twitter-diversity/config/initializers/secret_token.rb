1031261db61eb4cc225c4d82b88b5eb78cc63106
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-03-30 11:54:45 -0500

    creates rails framework

---
+++ twitter-diversity/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterDiversity::Application.config.secret_token = 'db7b5e29d8e338bc8734affda868e77f3ba88126cf39bacf327f595fdeb9dc815d4f99fc23fa1822b4db1e538d4e1a9701df8e71e19b08387a78c20e43408535'

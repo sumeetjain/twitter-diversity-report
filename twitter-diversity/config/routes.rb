a5005eb68144ed3df5680dc4a24f3f6fcf19c918
Alex <alexandrajmh@gmail.com>
2015-03-31 15:38:43 -0500

    adds dotenv and omniauth plus some basic scratch routes for twitter authentication

--- twitter-diversity/config/routes.rb
+++ twitter-diversity/config/routes.rb
@@ -1,4 +1,19 @@
 TwitterDiversity::Application.routes.draw do
+
+  get "/search" => "public#home", as: "home"
+  
+  get "/auth/twitter" => "public#call"
+
+  get "/auth/twitter/callback" => "public#return"
+
+  post "/results/:id" => "public#results"
+
+
+  
+  
+  
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

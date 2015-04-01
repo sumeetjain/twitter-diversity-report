948537d43d0276caee18275e5ddb8f25e11ae6ab
Andrew Strovers <ADStrovers@gmail.com>
2015-04-01 16:13:56 -0500

    Creates users controller and the 'new' view for it.

--- twitter-diversity/app/controllers/application_controller.rb
+++ twitter-diversity/app/controllers/application_controller.rb
@@ -16,7 +16,6 @@ class ApplicationController < ActionController::Base
       user_array.push user_object.twitter_handle
     end
 
-
     friend_array = client.friends(searched_twitter_handle).to_a
 
     friend_array.each_with_object([]) do |friend_object, matching_array|

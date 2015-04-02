6c1c8d7e1c513f1b9875d7be8f2d076a92693454
Andrew Strovers <ADStrovers@gmail.com>
2015-04-02 14:33:42 -0500

    refactors form and result processing

--- twitter-diversity/app/helpers/users_helper.rb
+++ twitter-diversity/app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def user_answer_field_label(answer_type)
+    case answer_type
+    when "Age"
+      "Birth year"
+    when "Income"
+      "How much $$$$?????"
+    end
+  end
 end

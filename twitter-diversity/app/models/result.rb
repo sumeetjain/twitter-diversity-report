61888383e662cb144132b6e0ec4c9e1672ff08aa
Alex <alexandrajmh@gmail.com>
2015-04-02 21:04:58 -0500

    adds actions for results controller and moves some functionality into Result methods. pushing for peer review

--- twitter-diversity/app/models/result.rb
+++ twitter-diversity/app/models/result.rb
@@ -28,7 +28,6 @@ class Result < ActiveRecord::Base
         matching_array.push friend_object
       end
     end
-    #matching_array is scoped within this block of code, cannot be accessed outside of it. only accessible right now because it is the final return of the method (and is set equal to a variable in our controller)
     matching_array
   end
   
@@ -90,5 +89,80 @@ class Result < ActiveRecord::Base
     end
     slice_hash
   end
+  
+  def edu_chart(demo_hash)
+    @edu_chart_data = [["Level Attained", "Count"]]
+      demo_hash["Education"].each do |k,v|
+        @edu_chart_data.push [k,v]
+      end
+  end
+      
+  def income_chart(demo_hash)
+    income_buckets = [["Under 15000", 0],
+                      ["15000 - 25000", 0],
+                      ["25000 - 35000", 0],
+                      ["35000 - 50000", 0],
+                      ["50000 - 75000", 0],
+                      ["75000 - 100000", 0],
+                      ["100000 - 150000", 0],
+                      ["Over 150000", 0]]
+  
+    demo_hash["Income"].each do |amount,count|
+      if amount < 15000
+        income_buckets[0][1] += count
+      elsif amount >= 15000 && amount < 25000
+        income_buckets[1][1] += count
+      elsif amount >= 25000 && amount < 35000
+        income_buckets[2][1] += count
+      elsif amount >= 35000 && amount < 50000
+        income_buckets[3][1] += count
+      elsif amount >= 50000 && amount < 75000
+        income_buckets[4][1] += count
+      elsif amount >= 75000 && amount < 100000
+        income_buckets[5][1] += count
+      elsif amount >= 100000 && amount < 150000
+        income_buckets[6][1] += count
+      elsif amount >= 150000
+        income_buckets[7][1] += count
+      end
+    end 
+  
+    @income_chart_data = [["Income", "Count"]]
+    income_buckets.each{ |b| @income_chart_data.push b }
+  end
+
+  def age_chart(demo_hash)
+    age_buckets = [["Under 18", 0],
+                  ["18-24", 0],
+                  ["25-34", 0],
+                  ["35-44", 0],
+                  ["45-54", 0],
+                  ["55-64", 0],
+                  ["65 and older", 0]]
+              
+    demo_hash["Age"].each do |birth_year,count|
+    age = Time.now.year - birth_year
+      if age < 18
+        age_buckets[0][1] += count
+      elsif age >= 18 && age < 24
+        age_buckets[1][1] += count
+      elsif age >= 25 && age < 34
+        age_buckets[2][1] += count
+      elsif age >= 35 && age < 44
+        age_buckets[3][1] += count
+      elsif age >= 45 && age < 54
+        age_buckets[4][1] += count
+      elsif age >= 55 && age < 64
+        age_buckets[5][1] += count
+      elsif age >= 65
+        age_buckets[6][1] += count
+      end
+    end
+
+    @age_chart_data = [["Age", "Count"]]
+    age_buckets.each{ |bucket| @age_chart_data << bucket }
+  end
+  
+  
 
 end

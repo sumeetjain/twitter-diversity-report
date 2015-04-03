7c24197006a221eece8d009de9d73f979dce276d
Crystal Cooling <cooling.cne@gmail.com>
2015-04-02 20:13:21 -0500

    moved fetch friend method to result model

--- twitter-diversity/app/models/result.rb
+++ twitter-diversity/app/models/result.rb
@@ -5,6 +5,33 @@ class Result < ActiveRecord::Base
   
   serialize :demo_hash, Hash
   
+  def build_result_hash(client, searched_twitter_handle)
+    matching_array = fetch_friend_matches(client, searched_twitter_handle)
+    friend_answers = get_friend_answers(matching_array)
+    demos = get_demo_types(friend_answers)
+    demo_hash = generate_demo_hash(demos, friend_answers)
+  
+  end
+  
+  def fetch_friend_matches(client, searched_twitter_handle)
+    
+    user_array = []
+    
+    User.select("twitter_handle").each do |user_object| 
+      user_array.push user_object.twitter_handle
+    end
+
+    friend_array = client.friends(searched_twitter_handle).to_a
+
+    friend_array.each_with_object([]) do |friend_object, matching_array|
+      if user_array.include?(friend_object.screen_name.downcase)
+        matching_array.push friend_object
+      end
+    end
+    #matching_array is scoped within this block of code, cannot be accessed outside of it. only accessible right now because it is the final return of the method (and is set equal to a variable in our controller)
+    matching_array
+  end
+  
   def get_friend_answers(m)
     friend_answers = []
     m.each do |friend_obj|
@@ -24,12 +51,14 @@ class Result < ActiveRecord::Base
   end
   
   def generate_demo_hash(demos, friend_answers)
+    demo_hash = {}
     demos.each do |d|
       sub_ans = subset_fa_by_demo(d,friend_answers)
       unique_ans = get_unique_answers(sub_ans)
       slice_hash = build_slice_hash(unique_ans, sub_ans)  #indirect SQL
       demo_hash[d] = slice_hash
     end
+    demo_hash
   end
   
   def subset_fa_by_demo(d,friend_answers)

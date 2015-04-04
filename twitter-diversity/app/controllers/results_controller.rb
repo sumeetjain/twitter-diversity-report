4584af86d452b555fa63ffcb7297afacbd1ab5ff
Alex <alexandrajmh@gmail.com>
2015-04-04 11:00:43 -0500

    Adds a reroute landing page btwn search click on homepage and viewing the results, later to be integrated more seamlessly ie not through a separate landing view. Next steps are to double check that, when someone clicks search and then clicks yes they will fill out their info, an existing user is directed to the view page of their info, but an entirely new user would be directed to the create/edit form. After that, the next step is to delay jobs on creating specific result view

--- twitter-diversity/app/controllers/results_controller.rb
+++ twitter-diversity/app/controllers/results_controller.rb
@@ -5,7 +5,7 @@ class ResultsController < ApplicationController
     
     client = Result.client
     
-    demo_hash = Result.build_result_hash(client, @twitter_handle)
+    demo_hash = Result.build_result_hash2(client, @twitter_handle)
 
     result = Result.create(searched_handle: @twitter_handle,
                         demo_hash: demo_hash)
@@ -22,13 +22,21 @@ class ResultsController < ApplicationController
     result = Result.create(searched_handle: @twitter_handle,
                         demo_hash: demo_hash)
     
-    
-    redirect_to "/results/#{result.id}"
+    if session[:screen_name] == nil
+      redirect_to "/reroute"
+      session[:result] = result
+    else
+      redirect_to "/results/#{result.id}"
+    end
   end
   
   def view
     session[:searched_twitter_handle] = nil
-    @result = Result.find(params[:id])  
+      @result = Result.find(params[:id])  
+  end
+  
+  def reroute
+    binding.pry
   end
   
 end

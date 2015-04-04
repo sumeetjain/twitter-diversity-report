364ecd3db25ef7271660ac91e844897715f13fb2
Crystal Cooling <cooling.cne@gmail.com>
2015-04-04 16:53:59 -0500

    adds demo models and chart details

---
+++ twitter-diversity/app/models/orientation.rb
@@ -0,0 +1,8 @@
+class Orientation < ActiveRecord::Base
+  attr_accessible :value
+
+  validates :value, presence: true  
+  validates :value, uniqueness: {case_sensitive: false}
+  
+  has_many :user_answers, as: :answer
+end

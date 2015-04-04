1c9c3720261adb30d6a08006667e5848232dc252
Alex <alexandrajmh@gmail.com>
2015-04-04 16:09:54 -0500

    adds flash errors on homepage for bad searches

--- twitter-diversity/app/controllers/results_controller.rb
+++ twitter-diversity/app/controllers/results_controller.rb
@@ -15,19 +15,24 @@ class ResultsController < ApplicationController
 
   def create
     client = Result.client
-    @twitter_handle = params[:twitter_handle]
+    if params[:twitter_handle] == nil || params[:twitter_handle] == ""
+      flash[:message] = "Oops. You didn't enter any information."
+      redirect_to "/"
+    else
+      @twitter_handle = params[:twitter_handle]
+    
+      demo_hash = Result.build_result_hash(client, @twitter_handle)
 
-    demo_hash = Result.build_result_hash(client, @twitter_handle)
-
-    result = Result.create(searched_handle: @twitter_handle,
+      result = Result.create(searched_handle: @twitter_handle,
                         demo_hash: demo_hash)
     
-    if session[:screen_name] == nil
-      redirect_to "/reroute"
-      session[:searched_for] = params[:twitter_handle]
-      session[:result] = result
-    else
-      redirect_to "/results/#{result.id}"
+      if session[:screen_name] == nil
+        redirect_to "/reroute"
+        session[:searched_for] = params[:twitter_handle]
+        session[:result] = result
+      else
+        redirect_to "/results/#{result.id}"
+      end
     end
   end
   
@@ -38,7 +43,13 @@ class ResultsController < ApplicationController
   end
   
   def reroute
-    binding.pry
+    if session[:result].demo_hash = {}
+      redirect_to "/"
+      flash[:message] = "Oh no! @#{session[:searched_for]} is not following anyone who's filled out information with us. Try another search:" 
+    else
+      render "/results/reroute"
+    end
+
   end
   
 end

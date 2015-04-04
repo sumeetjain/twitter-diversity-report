4f820b3168677d70ba93d0688e128dde0566978e
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-04-04 13:37:03 -0500

    adds media-queries to js for main page boxes

--- twitter-diversity/app/assets/javascripts/public.js
+++ twitter-diversity/app/assets/javascripts/public.js
@@ -18,10 +18,10 @@ window.onload = function(){
   document.getElementById("searchinfo").addEventListener("click", function(){
     if (document.getElementById("searchinfobox").style.display === "none") {
       document.getElementById("searchinfobox").style.display = "block";
-      document.getElementById("searchbox").style.height = "450px";
+      document.getElementById("searchbox").style.height = "585px";
     } else {
       document.getElementById("searchinfobox").style.display = "none";
-      document.getElementById("searchbox").style.height = "225px";
+      document.getElementById("searchbox").style.height = "300px";
     }
   });
   
@@ -29,12 +29,28 @@ window.onload = function(){
   document.getElementById("addinfo").addEventListener("click", function(){
     if (document.getElementById("addinfobox").style.display === "none") {
       document.getElementById("addinfobox").style.display = "block";
-      document.getElementById("infobox").style.height = "550px";
-    } else {
+      
+ 
+      if (window.matchMedia("(min-width: 994px)").matches) {
+        document.getElementById("infobox").style.height = "670px";
+      } else if (window.matchMedia("(min-width: 965px)").matches) {
+        document.getElementById("infobox").style.height = "745px";
+      } else if (window.matchMedia("(min-width: 814px)").matches) {
+        document.getElementById("infobox").style.height = "860px";
+      } else if (window.matchMedia("(min-width: 770px)").matches) {
+        document.getElementById("infobox").style.height = "975px"; 
+      } else if (window.matchMedia("(min-width: 663px)").matches) {
+        document.getElementById("infobox").style.height = "1075px";
+      } else if (window.matchMedia("(min-width: 500px)").matches) {
+        document.getElementById("infobox").style.height = "1100px";
+      } 
+      
+  } else {
       document.getElementById("addinfobox").style.display = "none";
-      document.getElementById("infobox").style.height = "225px";
+      document.getElementById("infobox").style.height = "300px";
     }
   });
   
   
 }
+

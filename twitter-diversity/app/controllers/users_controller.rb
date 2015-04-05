6b82b90f3a59e4bf51cdde3e6699af058c556e88
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-04-05 16:40:07 -0500

    lots of refactoring/reworking. genders should be good to go - need to replicate for teh others. also contains some style notes from the client.

--- twitter-diversity/app/controllers/users_controller.rb
+++ twitter-diversity/app/controllers/users_controller.rb
@@ -21,29 +21,58 @@ class UsersController < ApplicationController
       end
     end
     
-    #makes array of strings of each gender entry
-    params["user"]["user_answers_attributes"].each do |x, y|
-      if y["answer_type"] == "Gender"
-        @gender_array = y["value"].split("\r\n")
-      end
-    end
+    ############# ETHNICITY #################################
     
-    #removes gender entry from array
-    params["user"]["user_answers_attributes"].delete_if do |x, y|
-      y["answer_type"] == "Gender" && y["value"].include?("\r\n")
-    end
+    #makes array of strings of each ethnicity entry
+    # params["user"]["user_answers_attributes"].each do |x, y|
+    #   if y["answer_type"] == "Ethnicity"
+    #     if y["value"].include?("\r\n")
+    #       @ethnicity_array = y["value"].split("\r\n")
+    #
+    #       params["user"]["user_answers_attributes"].delete_if do |x, y|
+    #         y["answer_type"] == "Ethnicity" && y["value"].include?("\r\n")
+    #       end
+    #
+    #       @need_to_add = true
+    #
+    #     end
+    #   end
+    # end
+    #
+    # if @need_to_add
+    #   num = params["user"]["user_answers_attributes"].keys.last.to_i
+    #
+    #   @ethnicity_array.each do |ethnicity|
+    #     num += 1
+    #     params["user"]["user_answers_attributes"]["#{num.to_s}"] = {"answer_type"=>"Ethnicity", "value"=>"#{ethnicity.to_s}"}
+    #   end
+    # end
+  
+    ############# GENDER #################################
     
+    # #makes array of strings of each gender entry
+    # params["user"]["user_answers_attributes"].each do |x, y|
+    #   if y["answer_type"] == "Gender"
+    #     if y["value"].include?("\r\n")
+    #       @gender_array = y["value"].split("\r\n")
+    #
+    #       params["user"]["user_answers_attributes"].delete_if do |x, y|
+    #         y["answer_type"] == "Gender" && y["value"].include?("\r\n")
+    #       end
+    #
+    #       num = params["user"]["user_answers_attributes"].keys.last.to_i
+    #
+    #       @gender_array.each do |gender|
+    #         num += 1
+    #         params["user"]["user_answers_attributes"]["#{num.to_s}"] = {"answer_type"=>"Gender", "value"=>"#{gender.to_s}"}
+    #       end
+    #     end
+    #   end
+    # end
+    #
+    ############################################################
     
-    #will need to tweak for multiple ethnicites/orientations - just a test for now
-    num = params["user"]["user_answers_attributes"].keys.last.to_i
 
-    @gender_array.each do |gender|
-        num += 1
-        params["user"]["user_answers_attributes"]["#{num.to_s}"] = {"answer_type"=>"Gender", "value"=>"#{gender.to_s}"}
-    end
-        
-    # Works, but gets messed up right now because of how repopulates the edit form. I think once I make that reconcatenate, it 'll work fine. 
-    
     if params["user"]["user_answers_attributes"][education_index]["value"] == ""
       params["user"]["user_answers_attributes"][education_index]["value"] = params["education1"]
     end
@@ -60,41 +89,14 @@ class UsersController < ApplicationController
     user_answer_types = @user.user_answers.select("distinct answer_type")
     
     all_demos = UserAnswer.select("distinct answer_type").map{ |a| a.answer_type }
-      
-    #creates array of strings of answers selected
-    
-    gender_answers_array = []
-    
-    @user.user_answers.each do |answerobject|
-      if answerobject.answer_type == "Gender"
-        gender_answers_array.push answerobject.answer.value
-        @placeholder_gender_object = answerobject
-      end
-    end
-    
-    #gets string of all answer values joined by newline
-    
-    @gender_answers = gender_answers_array.join("\r\n")
-    
-    #removes gender objects from array
 
-    @user.user_answers.delete_if do |answerobject|
-      answerobject.answer_type == "Gender"
-    end
-    
-    #puts placeholder gender object back in
-    if @placeholder_gender_object
-      @user.user_answers.push @placeholder_gender_object
-    end
-            
     if @user.user_answers == []
       @user.user_answers.build(answer_type: "Education")
       @user.user_answers.build(answer_type: "Age")
       @user.user_answers.build(answer_type: "Income")
-      @user.user_answers.build(answer_type: "Gender")
-      @user.user_answers.build(answer_type: "Ethnicity")
-      @user.user_answers.build(answer_type: "Orientation")
     end
+    
+    @single_answers = @user.user_answers.where(answer_type: ["Education", "Age", "Income"])
     
   end
   
@@ -106,18 +108,28 @@ class UsersController < ApplicationController
     
     
     # deletes existing user gender answers from user_answers table so that doesn't make repeate entries
-    @user.user_answers.where(user_id: @user.id).where(answer_type: "Gender").each do |answer|
-      answer.destroy
-    end
+    # @user.user_answers.where(user_id: @user.id).where(answer_type: "Gender").each do |answer|
+    #   answer.destroy
+    # end
+    
+    # deletes existing user ethnicity answers from user_answers table so that doesn't make repeate entries
+    # @user.user_answers.where(user_id: @user.id).where(answer_type: "Ethnicity").each do |answer|
+    #   answer.destroy
+    # end
     
     # Add user ID to params, since we don't want users to be able to add it themselves in web inspector.
     params[:user][:user_answers_attributes].each do |k, h|
       h[:user_id] = @user.id
-    end 
+    end
     
-    @user.update_attributes(params[:user])
+    binding.pry 
+
+    if @user.update_attributes(params[:user]) #kicking up error: Couldn't find UserAnswer with ID=83 for User with ID=15
       
-    flash[:message] = "Your information has been added to our files; Any identifying information has been encrypted."
+      flash[:message] = "Your information has been added to our files; Any identifying information has been encrypted."
+    else 
+      flash[:errors] = @user.errors.to_a #add to page as if <ul> loop to show errors
+    end
 
     if session[:searched_for] == nil
       redirect_to "/users/#{session[:screen_name]}" # To change to results.
--- twitter-diversity/app/models/user.rb
+++ twitter-diversity/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :user_answers_attributes, :twitterid
+  attr_accessible :user_answers_attributes, :twitterid, :ethnicities_text_area
+  
+  validates :ethnicities_text_area, length: {maximum: 20}
   
   #validates :twitter_handle, presence: true
   validates :twitterid, presence: true
@@ -33,5 +35,38 @@ class User < ActiveRecord::Base
   
   accepts_nested_attributes_for :user_answers
 
+  # Returns gender values for this user as an Array.
+  def genders_as_array
+    genders.map do |g|
+      g.value
+    end
+  end
+  
+  # Returns ethnicity values for this user as an Array.
+  def ethnicities_as_array
+    ethnicities.map do |g|
+      g.value
+    end
+  end
+  
+  def ethnicities_text_area
+    ethnicities_as_array.join("\r\n")
+  end
+  
+  def ethnicities_text_area=(text_from_form_field) #pull out method body into separate private method below and then call for each setter for respecitve categories
+    self.ethnicities.delete_all
+    
+    users_inputs_array = text_from_form_field.split("\r\n")
+    
+    users_inputs_array.each do |e|
+      if !self.ethnicities_as_array.include?(e)
+        self.ethnicities << Ethnicity.find_or_create_by_value(e)
+      end
+    end
+  end
+  
+  def orientations_as_array
+    # TODO
+  end
 
 end

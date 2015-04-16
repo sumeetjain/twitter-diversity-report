07c0b967fe6fb7c3990f27fcff0831f56d44c3ea
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-04-15 19:57:15 -0500

    pushing for peer review; JS files currently overwrite one another.

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+window.onload = function(){
+  
+  document.getElementById("methodology").addEventListener("click", function(){
+    if (document.getElementById("methodologyinfobox").style.display === "none") {
+      document.getElementById("methodologyinfobox").style.display = "block";
+    } else {
+      document.getElementById("methodologyinfobox").style.display = "none";
+    }
+  });
+  
+};

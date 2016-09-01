c6b9a6acfd13100b37a5b1f3bbef124fc2ed5dc3
Sumeet Jain <sumeet@sumeetjain.com>
2016-09-01 09:56:39 -0500

    First commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require "webmock/rspec"
+
+# http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  config.expect_with :minitest, :rspec
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+
+  config.example_status_persistence_file_path = "tmp/rspec_examples.txt"
+  config.order = :random
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
+WebMock.disable_net_connect!(allow_localhost: true)

bb7d930f5c9fcf4b6fdefd304a2cfd5cccd2a170
Susan Tussing <susan@fionavar.net>
2016-09-02 14:39:37 -0500

    Add requirement for twitter id

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new
+  end
+
+  it 'is valid with a twitter id' do
+    @user.twitter_id = 123456790
+    expect(@user.valid?).to be true
+  end
+
+  it 'is invalid without a twitter id' do
+    @user.twitter_id = 
+    expect(@user.valid?).to be false
+  end
 end

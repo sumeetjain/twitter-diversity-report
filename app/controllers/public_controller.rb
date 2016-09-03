f93aa30ac9b8b933ed10a6e2671e974ea729568b
Alexander Noto <noto.alex.j@gmail.com>
2016-09-03 13:27:27 -0500

    adding methods

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,8 +3,41 @@ class PublicController < ApplicationController
   end
 
   def failure
+    redirect_to "/auth/twitter"
   end
-  
+
   def return
   end
+end
+
+
+class PublicController < ApplicationController
+
+
+
+
+
+
+  def return
+    session[:screen_name] = oauth_hash['info']['nickname']
+    session[:twitter_id] = oauth_hash['uid'].to_i
+
+    #user = User.find_by_twitter_handle(session[:screen_name].downcase)
+    user = User.find_or_create_by_twitterid(session[:twitter_id])
+
+    if user
+      redirect_to "/users/#{session[:screen_name]}/edit"
+    end
+  end
+
+  def result
+  end
+
+  protected
+
+  def oauth_hash
+    request.env['omniauth.auth']
+  end
+
+
 end

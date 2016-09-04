69b68fad3afb5987085f083b2c49ec95539be979
Alexander Noto <noto.alex.j@gmail.com>
2016-09-04 15:16:03 -0500

    added index, need to separate styling out

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,7 @@ class PublicController < ApplicationController
   def index
   end
 
+#Don't see anywhere where this method is actually called
   def failure
     redirect_to "/auth/twitter"
   end
@@ -13,7 +14,6 @@ class PublicController < ApplicationController
     session[:screen_name] = oauth_hash['info']['nickname']
     session[:twitter_id] = oauth_hash['uid'].to_i
 
-    #user = User.find_by_twitter_handle(session[:screen_name].downcase)
     user = User.find_or_create_by(twitterid:(session[:twitter_id]))
 
     if user

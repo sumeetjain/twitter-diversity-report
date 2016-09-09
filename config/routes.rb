cbdc58240991c80e504c1f0b64b9836d6ad59ba9
Alexander Noto <noto.alex.j@gmail.com>
2016-09-09 13:19:50 -0500

    removed old routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,6 @@ Rails.application.routes.draw do
 
     get "/users/self/result" => 'users#selfresult'
 
-    # put "/users/:id" => 'users#save', as: :user
-
-    # delete "/users/:id" => 'users#delete' #protected
-
-    # get "/users/:id" => 'users#view' #protected
-
     ########## RESULTS ###########################
     post "/reroute" => 'searchresults#reroute'
 
@@ -29,24 +23,10 @@ Rails.application.routes.draw do
 
     get "/result/:twitter_handle" => 'searchresults#reroute'
 
-    # get "/current" => 'results#current'
-    #
-    # get "/current/:screen_name" => 'results#current' #redirects to /:id
-    #
-    # get "/results/:id" => 'results#view' #pulls from results table
-    #
-    # post "/results" => 'results#create' #generates results record to populate /:id, make sure to clear session after this loads
-    #
-    # get "/reroute" => 'results#reroute' #TODO would like to change to a post route
-
     ######### OMNIAUTH ###########################
 
     get "/auth/twitter/callback" => "public#returnFromAuth"
 
     get "/auth/failure" => "public#failure"
-
-    ######### BAD USER ENTRY REROUTING ###########
-
-    # get "/:screen_name" => 'results#current'
 
 end
